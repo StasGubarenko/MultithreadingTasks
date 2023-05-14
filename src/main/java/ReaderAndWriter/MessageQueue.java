5d6eb1f70febd565d4e5f7923fa9641f48929cdf
StasGubarenko <120662529+StasGubarenko@users.noreply.github.com>
2023-05-14 19:17:59 +0600

    Update MessageQueue.java

--- src/main/java/ReaderAndWriter/MessageQueue.java
+++ src/main/java/ReaderAndWriter/MessageQueue.java
@@ -3,6 +3,6 @@ package ReaderAndWriter;
 import java.util.LinkedList;
 
 public class MessageQueue {
-    //Общий ресурс, которому будут иметь 2 потока
+    //Общий ресурс, которому будут иметь доступ 2 потока
     static LinkedList<Integer> queue = new LinkedList<>();
 }
